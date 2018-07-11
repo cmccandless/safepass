25ba14be655fb6b44faef5591c9a8dc9f2d51b0a
Corey McCandless <corey.mccandless@atlasied.com>
2018-07-11 11:21:58 -0400

    set User-Agent

--- safepass/__init__.py
+++ safepass/__init__.py
@@ -8,7 +8,12 @@ API_BASE = 'https://api.pwnedpasswords.com/range/'
 def safepass(passwd):
     passwd = passwd.encode()
     hashed = hashlib.sha1(passwd).hexdigest().upper()
-    page = requests.get(API_BASE + hashed[:5])
+    page = requests.get(
+        API_BASE + hashed[:5],
+        headers={
+            'User-Agent': 'safepass 1.0.1'
+        }
+    )
     for line in page.text.split('\n'):
         suffix, count = line.split(':')
         count = int(count)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="safepass",
-    version="1.0.0",
+    version="1.0.1",
     author="Corey McCandless",
     author_email="crm1994@gmail.com",
     description=(
